9e077b8c681d391d22cbca78c7cff111c9584406
Oroko <aoroko09@gmail.com>
2021-03-23 21:50:57 -0400

    Add before action for user authentication and added fild to the devise user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_230042) do
+ActiveRecord::Schema.define(version: 2021_03_24_014711) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2021_03_23_230042) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
