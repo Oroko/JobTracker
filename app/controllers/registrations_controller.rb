1072c00af4d29cb5d11e621dfe8f3cdddf77cb8d
Oroko <aoroko09@gmail.com>
2021-03-24 00:15:11 -0400

    Add new fields to the user model and styled login, sign up and forgot password forms

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class RegistrationsController < Devise::RegistrationsController
+
+  
   private
 
   def sign_up_params
@@ -9,5 +11,5 @@ class RegistrationsController < Devise::RegistrationsController
 
   def account_update_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
- end
+  end
 end
