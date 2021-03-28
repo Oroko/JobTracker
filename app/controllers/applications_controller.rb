77d76b25add9e5d0f9768034963e713bacb73683
Oroko <aoroko09@gmail.com>
2021-03-28 11:33:55 -0400

    Add the application controller with the index, create and show actions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,13 +1,37 @@
+# frozen_string_literal: true
+
 class ApplicationsController < ApplicationController
-  
-  def new 
+  def new
     @job = Job.find_by_id(params[:job_id])
     @application = @job.applications.build
 
-    #An application belongs to a job
-  end 
+    # An application belongs to a job
+  end
 
-  def index 
+  def show
+    @application = Application.find(params[:id])
+  end
 
+  def index
+    @applications = if @job = Job.find_by_id(params[:job_id])
+                      @job.applications
+                    else
+                      Application.all
+                    end
+  end
+
+  def create
+    @application = current_user.applications.build(applications_params)
+    if @application.save
+      redirect_to application_path(@application)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def applications_params
+    params.require(:application).permit(:job_id, :date_of_application, :notes)
   end
 end
