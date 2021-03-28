1f22bf548a86f6b51f4020945bd157d21a3220c6
Oroko <aoroko09@gmail.com>
2021-03-27 21:04:45 -0400

    Add a nested resource application new for a job

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationsController < ApplicationController
   
   def new 
+    @job = Job.find_by_id(params[:job_id])
+    @application = @job.applications.build
 
+    #An application belongs to a job
   end 
 
   def index 
-    
+
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,9 +1,15 @@
-class Job < ApplicationRecord 
-  belongs_to :user , optional: true #creator of it 
+# frozen_string_literal: true
+
+class Job < ApplicationRecord
+  belongs_to :user, optional: true # creator of it
   belongs_to :company, optional: true
-  has_many :applications 
-  has_many :users, through: :applications #people who have applied to jobs
+  has_many :applications
+  has_many :users, through: :applications # people who have applied to jobs
 
-  accepts_nested_attributes_for :company
+  # accepts_nested_attributes_for :company
 
+  def company_attributes=(attributes)
+    Company.find_or_create_by(attributes) unless
+    name.empty
+  end
 end
